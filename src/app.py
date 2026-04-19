074ebb1292cc557206a35436cf9d53d6d8dcf544
ramesh4500 <ramesh.amadoru@gmail.com>
2026-04-19 02:20:54 -0400

    Updated "ArgoCD" sync method and made a change to app.py

--- src/app.py
+++ src/app.py
@@ -44,7 +44,7 @@ def info():
     return jsonify({
         'time': current_time,
         'hostname': socket.gethostname(),
-        'message': "Your Application is running great and you are doing awesome work!",
+        'message': "Your Application is running great and you are doing awesome work! <3",
         'deployed_on': 'kubernetes'
     })
 
